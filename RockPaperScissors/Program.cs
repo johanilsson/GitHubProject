b7813bb3050b0491e7cd453598034f6c93a8dfd6
johanilsson <jnilsson.malmo@gmail.com>
2023-09-27 10:33:51 +0200

    Cleared begining and random

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Player 1 name: ");
+string p1 = Console.ReadLine();
+Console.WriteLine("-------------------------");
+Console.Write("Player 2 name: ");
+string p2 = Console.ReadLine();
+
+Console.Clear();
+
+Console.WriteLine("Welcome " + p1 + " & " + p2);
+Console.WriteLine("Lets play a game of Rock Papper Sissors");
+
+
+Console.Clear();
+
+// Random 
+
+Random rnd = new Random();
+
+int begin = (rnd.Next(0, 10));
+
+Console.WriteLine(begin);
