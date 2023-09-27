02cbef2bbd8ebbe27e835ed86b5bb8f64da1132a
johanilsson <jnilsson.malmo@gmail.com>
2023-09-27 13:45:26 +0200

    HA! adding class and Enum

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -1,6 +1,7 @@
 ﻿Random rnd = new Random();
 int begin = (rnd.Next(0, 10));
 string beginersIs;
+string[] choice = { "Rock", "Papper", "Sissor" };
 
 Console.Write("Player 1 name: ");
 string player1 = Console.ReadLine();
@@ -13,6 +14,7 @@ Console.Clear();
 Console.WriteLine("Welcome " + player1 + " & " + player2);
 Console.WriteLine("Lets play a game of Rock Papper Sissors");
 
+Thread.Sleep(3000);
 
 Console.Clear();
 
@@ -26,5 +28,25 @@ else
     {
     beginersIs = player2;
     }
-Console.WriteLine(begin);
+
+// begining
+
 Console.WriteLine(beginersIs + " <- Begins!");
+
+Thread.Sleep(1000);
+
+Console.WriteLine("------------------");
+
+//choice
+
+for (int i = 0; i < choice.Length; i++)
+{
+    Console.WriteLine(choice[i]);
+    
+}
+
+
+Console.WriteLine("------------------");
+Console.WriteLine(beginersIs + "Make a choice! ");
+string firstGame = Console.ReadLine();
+
